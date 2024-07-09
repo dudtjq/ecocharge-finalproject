c22adaec856a1577cc591097502edab0abe85f20
wonjinseop <wonjinseop7@naver.com>
2024-07-09 12:38:56 +0900

    Docs: 지도 마커기능 수정

--- src/main/java/com/example/demo/controller/ChargeSpotController.java
+++ src/main/java/com/example/demo/controller/ChargeSpotController.java
@@ -18,11 +18,15 @@ public class ChargeSpotController {
     private final ChargeSpotService chargeSpotService;
     
     @GetMapping("/marker")
-    public ResponseEntity<?> createChargeSpotMarker(@RequestParam(name = "lat") double lat, @RequestParam(name = "lng") double lng) {
+    public ResponseEntity<?> createChargeSpotMarker(
+            @RequestParam(name = "lat") double lat,
+            @RequestParam(name = "lng") double lng,
+            @RequestParam(name = "zoom") int zoom
+    ) {
         
-        log.info("/chargespot/marker?lat={}&lng={} GET!!", lat, lng);
+        log.info("/chargespot/marker?lat={}&lng={}&zoom={} GET!!", lat, lng, zoom);
         
-        List<ChargeSpotMarkerResponsDTO> markerList = chargeSpotService.getMarker(lat, lng);
+        List<ChargeSpotMarkerResponsDTO> markerList = chargeSpotService.getMarker(lat, lng, zoom);
         
         return ResponseEntity.ok().body(markerList);
     }
