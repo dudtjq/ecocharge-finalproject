296d592ba41a8d12ec90b1edc7415cfae0b22b44
minah8 <o_uo_o@naver.com>
2024-07-09 14:37:43 +0900

    Fix: 후기 수정 시 권한이 있는 유저인지 확인하는 코드 추가

--- src/main/java/com/example/demo/service/ReservationService.java
+++ src/main/java/com/example/demo/service/ReservationService.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -58,6 +59,7 @@ public class ReservationService {
                 reservationRepository.save(reservation);
             }
         }
+        reservationList.sort(Comparator.comparing(Reservation::getReservationDate).reversed());
         log.info("reservationList: {}", reservationList);
         List<ReservationResponseDTO> dtoList = reservationList.stream()
                 .map(ReservationResponseDTO::new)
